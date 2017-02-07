049600e20c2669546191ee96788d9995fa80b12e
Dan Lindsay <dlindsay72@hotmail.com>
2017-02-06 20:29:38 -0500

    Basic ui done, implemented tableView and embedded main viewCOntroller in a nav controller

---
+++ PassThePic/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  PassThePic
+//
+//  Created by Dan Lindsay on 2017-02-06.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
--- PassThePic/ViewController.swift
+++ PassThePic/ViewController.swift
@@ -8,16 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let reuseIdentifier = "tableViewCell"
+    var dummyObjects = ["hi", "there", "I'm", "awesome!"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var newCell = UITableViewCell()
+        
+        if let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) {
+            newCell = cell
+        }
+        newCell.textLabel?.text = dummyObjects[indexPath.row]
+        
+        return newCell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+       return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyObjects.count
     }
 
 
